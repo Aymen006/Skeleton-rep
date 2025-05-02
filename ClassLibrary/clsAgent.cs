4f88488d5c9db053f65cc58fdd0fbab588464055
p2857641 <p2857641@GWHXX474WIN011W>
2025-05-02 14:55:51 +0100

    pushing the testing code

---
+++ ClassLibrary/clsAgent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsAgent
+    {
+        public string Description { get; set; }
+
+        public string Valid(string nDescription)
+        {
+            if (nDescription.Length < 1)
+            {
+                return "Description cannot be blank";
+            }
+            if (nDescription.Length > 50)
+            {
+                return " Description cannot be more than 50 char";
+            }
+            else
+            {
+                return "";
+            }
+            
+        }
+    }
+}
---
+++ Testing1/tstAgent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstAgent
+    {
+        //good test data
+        private string nDescription = " THis is the Marketing AI Agent ";
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsAgent AAGENT = new clsAgent();
+            Assert.IsNotNull(AAGENT);
+
+        }
+
+        [TestMethod]
+        public void DescriptionOK()
+        {
+            clsAgent AAGENT = new clsAgent();
+            string TestData = nDescription;
+            AAGENT.Description = TestData;
+            Assert.AreEqual(AAGENT.Description, TestData);
+
+        }
+
+        [TestMethod]
+        public void ValidOK()
+        {
+            clsAgent AAGENT = new clsAgent();
+            string Error = " ";
+            string TestData = nDescription;
+            Error = AAGENT.Valid(nDescription);
+            Assert.AreEqual(Error,"");
+
+        }
+
+        [TestMethod]
+        public void MinLessOne()
+        {
+            clsAgent AAGENT = new clsAgent();
+            string Error = " ";
+            string TestData = "";
+            Error = AAGENT.Valid(TestData);
+            Assert.AreNotEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void MaxPlusOne()
+        {
+            clsAgent AAGENT = new clsAgent();
+            string Error = " ";
+            string TestData = "";
+            TestData = TestData.PadLeft(51,'*' );
+            Error = AAGENT.Valid(TestData);
+            Assert.AreNotEqual(Error, "");
+
+        }
+    }
+}
+
