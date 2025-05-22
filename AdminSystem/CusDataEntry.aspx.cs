1117a79036a64ba5bed0af65c29d1a1b0647e8ac
rehmanayub <P2759159@my365.dmu.ac.uk>
2025-05-22 23:04:57 +0100

    Basic Front end

--- AdminSystem/CusDataEntry.aspx.cs
+++ AdminSystem/CusDataEntry.aspx.cs
@@ -1,14 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        
+    }
 
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        clsCustomer customer = new clsCustomer();
+
+        
+        customer.CustomerName = txtCustomerName.Text;
+
+        int customerId;
+        if (int.TryParse(txtCustomerId.Text, out customerId))
+        {
+            customer.CustomerId = customerId;
+        }
+        else
+        {
+            
+            customer.CustomerId = 0;
+        }
+
+        customer.CustomerEmail = txtCustomerEmail.Text;
+        customer.SubsicriptionId = txtSubscriptionId.Text; 
+        customer.PaymentStatus = txtPaymentStatus.Text;
+
+       
+        customer.CreatedAt = txtDate.SelectedDate;
+
+       
+        Session["customer"] = customer;
+
+        
+        Response.Redirect("CusViewer.aspx");
     }
 }
