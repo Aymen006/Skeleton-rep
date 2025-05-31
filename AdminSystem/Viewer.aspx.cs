3b76f9205f1d3e02f8783ee720609cf3b190777d
Aymen <118563178+Aymen006@users.noreply.github.com>
2025-05-31 16:57:43 +0100

    Create Viewer.aspx.cs

---
+++ AdminSystem/Viewer.aspx.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+
+public partial class _1Viewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //create a new instance clsAgent
+        clsAgent AnAgent = new clsAgent();
+        //Get the data from the session object
+        AnAgent = (clsAgent)Session["AnAgent"];
+        //Display the agent name for this entry
+        Response.Write(AnAgent.AgentName);
+        Response.Write(AnAgent.Descr);
+        Response.Write(AnAgent.Category);
+        Response.Write(AnAgent.IntegrationType);
+        Response.Write(AnAgent.Status);
+        Response.Write(AnAgent.UpdatedAt);
+        Response.Write(AnAgent.Price);
+    }
+}
